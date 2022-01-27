8c2912472cd3e8322a0a9108f6e5f85de062b1dc
elisemizerak <emizerak@hamilton.edu>
2022-01-27 16:33:46 +0000

    finishhomework

--- Activity1.R
+++ Activity1.R
@@ -1,4 +1,33 @@
  # Activity and Homework 1
 
+6*6
 
+
+
+myVec <- c(3,45,63,5,2)
+
+myData <- data.frame(col1=c(4,3),
+                     col2=c(2,5))
+
+myVec*10
+
+########### In class prompt 1 ----
+# Prompt 1
+
+snow_depth <- c(2.5,3,5,4.5)
+
+snow_depth*2.54
+
+# Prompt 2
+
+peak_names <- c("Mount Haystack", "Mount Skylight", "Dix Mountain", "Gray Peak")
+elevations <- c(4960, 4926, 4857, 4840)
+climb_ascent <- c(3570, 4265, 2800, 4178)
+round_trip_length <- c(17.8, 17.9, 13.2, 16)
+
+most_difficult_peaks <- data.frame(name = peak_names,
+                                   elevation = elevations,
+                                   ClimbAscent = climb_ascent,
+                                   roundtrip = round_trip_length)
+  
 
